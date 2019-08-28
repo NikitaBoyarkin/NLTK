bf56b18d749a88c21d1e3c0756ac834427e55539
NikitaBoyarkin <boyarkin_yandex@mail.ru>
2019-08-28 19:59:26 +0300

    commit

---
+++ tokenizing.py
@@ -0,0 +1,12 @@
+import nltk
+nltk.download()
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = "Hello Mr. Smith, how are you doing today? The weather is great and Python is awesome. \
+              The sky is pinkish-blue. You should not eat cardboard."
+
+print(sent_tokenize(example_text))
+
+print(word_tokenize(example_text))
+
+
